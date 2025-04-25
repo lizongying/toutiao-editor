9e25cc4bd4272249e8d95d24926cf522f66ce372
Li ZongYing <lizongying@msn.com>
2025-04-25 10:39:04 +0800

    docs

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
         })
     ],
     publicDir: 'public',
-    base: '/toutiao-editor/',
+    base: './',
     build: {
         rollupOptions: {
             input: {
